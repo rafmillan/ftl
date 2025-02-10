b4dbcc4a62034158740295f0f82dff80a4303cf0
rafmillan <rafael.k.millan@gmail.com>
2025-02-09 20:37:06 -0800

    added basic wear level test case

---
+++ test/test_wl_basic.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include "nand.h"
+
+int main(void)
+{
+    uint8_t rbuf[PAGE_SIZE];
+    uint8_t wbuf[PAGE_SIZE];
+
+    memset(wbuf, 0xAA, PAGE_SIZE);
+
+    nand_init();
+    uint32_t lba = 0;
+
+    // Wear down block 0
+    nand_block_erase(0);
+
+    write_lba(0, wbuf);
+
+    uint32_t blk, pg;
+    l2p_lookup(0, &blk, &pg);
+
+    if (blk == 0) {
+        printf("%s [FAIL]\n", __FILE__);
+        printf("Write to unexpected block! Expected: %d, Actual %d\n", 0, blk);
+    }
+
+
+    printf("%s [PASS]\n", __FILE__);
+    return 0;
+}
